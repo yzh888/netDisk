71b9b73159963015920ecb038bd090e742e6f8d7
yzh888 <1213790892@qq.com>
2020-10-24 22:08:28 +0800

    剩余容量实现

--- egg-example/app/controller/user.js
+++ egg-example/app/controller/user.js
@@ -126,6 +126,15 @@ class UserController extends Controller {
         }
         ctx.apiSuccess('退出登录成功');
     }
+
+    //剩余容量
+    async getSize() {
+        const { ctx, service } = this
+        return ctx.apiSuccess({
+            total_size: ctx.authUser.total_size,
+            used_size: ctx.authUser.used_size,
+        })
+    }
 }
 
 module.exports = UserController;
--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -15,4 +15,6 @@ module.exports = app => {
   router.post('/logout', controller.user.logout);
   //上传文件
   router.post('/upload', controller.file.upload);
+  //剩余容量
+  router.get('/getsize', controller.user.getSize)
 };
