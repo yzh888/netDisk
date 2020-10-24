cfa80b904010f173713989ec23134a6d83d661b0
yzh888 <1213790892@qq.com>
2020-10-24 22:32:30 +0800

    创建文件夹实现

--- egg-example/app/controller/file.js
+++ egg-example/app/controller/file.js
@@ -89,6 +89,7 @@ class FileController extends Controller {
     ctx.apiFail('上传失败')
   }
 
+  //文件列表
   async list() {
     const { ctx, app }= this
     const user_id = ctx.authUser.id
@@ -140,6 +141,44 @@ class FileController extends Controller {
     ctx.apiSuccess({
       rows,
     })
+  }
+
+  //创建文件夹
+  async createdir(){
+    const { ctx, app } = this
+    const user_id = ctx.authUser.id
+
+    ctx.validate({
+      file_id:{
+        required: true,
+        type: 'int',
+        defValue: 0,
+        desc: '目录id',
+      },
+      name:{
+        required:true,
+        type:'string',
+        desc:'文件夹名称',
+      },
+    })
+
+    let { file_id, name } = ctx.request.body
+
+    //验证目录id是否存在
+    if(file_id){
+      await this.service.file.isDirExist(file_id)
+    }
+
+    let res = await app.model.File.create({
+      name,
+      file_id,
+      user_id,
+      isdir:1,
+      size:0,
+    })
+
+    ctx.apiSuccess(res)
+
   }
 }
 
