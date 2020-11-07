9784136e10dbdac023010ef577de08535b8f2197
yzh888 <1213790892@qq.com>
2020-11-07 19:49:53 +0800

    删除和编辑管理员实现

--- live-stream-api/app/router.js
+++ live-stream-api/app/router.js
@@ -63,4 +63,10 @@ module.exports = (app) => {
   router.post('/admin/manager', controller.admin.manager.save)
   //管理员列表
   router.get('/admin/manager', controller.admin.manager.index);
+  // 编辑管理员
+  router.get('/admin/manager/edit/:id', controller.admin.manager.edit);
+  // 删除管理员
+  router.get('/admin/manager/delete/:id', controller.admin.manager.delete);
+  // 更新接口（编辑和删除公用）
+  router.get('/admin/manager/:id', controller.admin.manager.update);
 };
